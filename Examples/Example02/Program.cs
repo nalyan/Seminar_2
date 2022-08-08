dd9d4d97ed9cd0e9c5d7b1ab74f02b4193084a4b
ilnar <ilnar-sov@mail.ru>
2022-08-08 23:02:27 +0300

    Задача по нахождению сектора координат

---
+++ Examples/Example02/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Clear();
+Console.WriteLine ("Введите координату x, не равную нулю: ");
+int x = int.Parse (Console.ReadLine());
+Console.WriteLine ("Введите координату y, не равную 0: ");
+int y = int.Parse (Console.ReadLine());
+
+if (x>0 && y>0)
+{
+    Console.WriteLine ("Первая четверть.");
+}
+else if (x<0 && y>0)
+{
+    Console.WriteLine ("Вторая четверть.");
+}
+else if (x<0 && y<0)
+{
+    Console.WriteLine ("Третья четверть.");
+}
+else if (x>0 && y<0)
+{
+    Console.WriteLine ("Четвертая четверть.");
+}

b3bdde39e35134ab012badc9e7b3e9b54dc5c2d5
ilnar <ilnar-sov@mail.ru>
2022-08-08 22:57:06 +0300

    Задача по определению кратности

---
+++ Examples/Example01/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Clear();
+
+Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+
+if (number % 23 == 0 && number % 7 == 0)
+{
+    Console.WriteLine("Число " +number+ " кратно 7 и 23");
+}
+else if (number % 7 == 0)
+{
+    Console.WriteLine("Число " +number+ " кратно 7");
+}
+else if (number % 23 == 0)
+{
+    Console.WriteLine("Число " +number+ " кратно 23");
+}
+else
+{
+    Console.WriteLine("Число " +number+ " не кратно 7 и 23");
+}

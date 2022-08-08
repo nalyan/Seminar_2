54454d094a58e806c598fbde20aee0b796a1a278
ilnar <ilnar-sov@mail.ru>
2022-08-08 23:04:46 +0300

    Задача на возведение в степень

---
+++ Examples/Example04/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+
+int i = 1;
+Console.WriteLine("Введите число N: ");
+int N = int.Parse(Console.ReadLine());
+
+for (i=1; i<=N; i++)
+{
+    Console.WriteLine($"{i}*{i}={i*i}");
+}

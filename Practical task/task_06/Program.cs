35213d85d008e17f5321fab81224423259186f0e
ilnar <ilnar-sov@mail.ru>
2022-08-10 22:20:12 +0300

    Задача 6

---
+++ Practical task/task_06/Program.cs
@@ -0,0 +1,12 @@
+﻿// Найти сумму чисел от 1 до А
+Console.Clear();
+
+Console.WriteLine("Введите число: ");
+int A = int.Parse(Console.ReadLine());
+int count = 1;
+int sum = 0;
+for (count=1; count<=A; count++)
+{
+    sum = sum + count;
+}
+Console.WriteLine($"Сумма чисел от 1 до {A} = {sum}");

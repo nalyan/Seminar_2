abe706ea73a1290336ad9d78e0ebf7fb558e81e5
ilnar <ilnar-sov@mail.ru>
2022-08-10 23:55:21 +0300

    Задача 9

---
+++ Practical task/task_09/Program.cs
@@ -0,0 +1,12 @@
+﻿// Написать программу вычисления произведения чисел от 1 до N
+Console.Clear();
+
+Console.WriteLine("Введите число: ");
+int N = int.Parse(Console.ReadLine());
+int k = 1;
+long factorial = 1;
+for (k=1; k<=N; k++)
+{
+    factorial = factorial * k;
+}
+Console.WriteLine($"Произведение чисел от 1 до {N} = {factorial}");

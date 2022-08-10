4f1dc4f45a624be197e7027077deba31f4c428c9
ilnar <ilnar-sov@mail.ru>
2022-08-10 22:06:32 +0300

    Задача 5

---
+++ Practical task/task_05/Program.cs
@@ -0,0 +1,10 @@
+﻿// Найти кубы чисел от 1 до N
+Console.Clear();
+
+Console.WriteLine("Введите число: ");
+int N = int.Parse(Console.ReadLine());
+int k = 1;
+for (k = 1; k<=N; k++)
+{
+    Console.WriteLine($"{k}^3 = {k*k*k}");
+}

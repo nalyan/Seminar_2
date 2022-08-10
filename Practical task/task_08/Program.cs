3a14b30e8bf5cbef23a18151c80a4bb9af83de60
ilnar <ilnar-sov@mail.ru>
2022-08-10 23:34:55 +0300

    Задача 8

---
+++ Practical task/task_08/Program.cs
@@ -0,0 +1,18 @@
+﻿// Подсчитать сумму цифр в числе
+Console.Clear();
+
+Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+int figure = 0;
+int result = 0;
+int k = 10;
+int count = Math.Abs(number);
+
+while (count >= 1)
+{
+    figure = number % k / (k / 10);
+    result = result + figure;
+    k = k * 10;
+    count = count / 10;
+}
+Console.WriteLine(Math.Abs(result));

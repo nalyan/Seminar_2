34aafdcb64f4f6542d9269017992a20a147b071b
ilnar <ilnar-sov@mail.ru>
2022-08-10 20:37:49 +0300

    Задача 3 решена

---
+++ Practical task/task_03/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задать номер четверти, показать диапазоны для возможных координат
+Console.Clear();
+
+Console.Write("Введите номер четверти: ");
+int number = int.Parse(Console.ReadLine());
+string coord = "";
+if (number < 1 || number > 4) Console.WriteLine("Нет такой четверти");
+else
+{
+    if (number == 1) coord = "x > 0 ; y > 0";
+    else if (number == 2) coord = "x < 0 ; y > 0";
+    else if (number == 3) coord = "x < 0 ; y < 0";
+    else if (number == 4) coord = "x > 0 ; y < 0";
+
+    Console.WriteLine($"Для {number} четверти соответствуют слудующие диапазоны координат: [{coord}]");
+}
+
